3ef318830a72c524ab2d8b1cdb1c897bae210698
Aria Stewart <aredridel@dinhe.net>
2014-10-06 16:57:12 -0400

    Export app and test it without listening

--- test/index.js
+++ test/index.js
@@ -1,37 +1,20 @@
-/*global describe:false, it:false, beforeEach:false, afterEach:false*/
-
+/*global describe:false, it:false, before:false */ 
 'use strict';
 
 
-var kraken = require('kraken-js'),
-    express = require('express'),
-    request = require('supertest');
+var request = require('supertest');
 
 
 describe('/', function () {
 
-    var app, mock;
+    var app = require('../');
 
-
-    beforeEach(function (done) {
-        app = express();
+    before(function (done) {
         app.on('start', done);
-        app.use(kraken({
-            basedir: process.cwd()
-        }));
-
-        mock = app.listen(1337);
-
     });
-
-
-    afterEach(function (done) {
-        mock.close(done);
-    });
-
 
     it('should say "hello"', function (done) {
-        request(mock)
+        request(app)
             .get('/')
             .expect(200)
             .expect('Content-Type', /html/)
