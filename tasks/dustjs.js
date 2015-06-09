497f700562609a4ceca4fbb852b86dcfa61bb09e
Aria Stewart <aredridel@dinhe.net>
2015-06-09 15:44:41 -0400

    Rip out localizr
    Also fixes the template names encoded into .js templates by dustc, since
    we'd overridden them badly.

--- tasks/dustjs.js
+++ tasks/dustjs.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var path = require('path');
 
 module.exports = function dustjs(grunt) {
 	// Load task
@@ -11,7 +12,7 @@ module.exports = function dustjs(grunt) {
 	        files: [
 	            {
 	                expand: true,
-	                cwd: 'tmp/',
+	                cwd: 'public/templates/',
 	                src: '**/*.dust',
 	                dest: '.build/templates',
 	                ext: '.js'
@@ -19,16 +20,7 @@ module.exports = function dustjs(grunt) {
 	        ],
 	        options: {
 	            fullname: function (filepath) {
-	                var path = require('path'),
-	                    name = path.basename(filepath, '.dust'),
-	                    // Hardcoded to forwards slash on purpose. This is due to the way that grunt handles globbing.
-	                    // Patterns like **/*.dust are always expanded using '/' with no consideration to the host OS
-	                    // separator. This caused issues when trying to build on win_32
-	                    parts = filepath.split('/'),
-	                    fullname = parts.slice(3, -1).concat(name);
-
-	                // Hardcoded to forwards slash on purpose. Keeps compatibility on win_32
-	                return fullname.join('/');
+                    return path.relative('public/templates/', filepath).replace(/[.]dust$/, '');
 	            }
 	        }
 	    }
